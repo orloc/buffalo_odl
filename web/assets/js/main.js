ba53476a411307998f1fbb349a08004a654472ab
Grant <grant.tepper@gmail.com>
2013-07-15 01:44:32 -0400

    updated git ignore added more assets refined margins and css

---
+++ web/assets/js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+	$('span.link-over').each(function() {
+		$(this).hide();
+	});
+
+	$('#navigation div a').each(function(){
+		$(this).hover(
+			function(){
+				$(this).find('img').hide().closest('a').find('.link-over').show();
+			},
+			function(){
+				$(this).find('.link-over').hide().closest('a').find('img').show();
+			}
+		);
+	});
+
+	window.scroll(function () { 
+		console.log('bitches');
+		var div = $('#breadcrumbs');
+		if (this.scrollTop != 0 ) {
+			if (!(div.hasClass('navbar-fixed-top'))){
+				div.addClass('navbar-fixed-top');
+			}	
+		} else if ( div.hasClass('navbar-fixed-top')) {
+			div.removeClass('navbar-fixed-top');
+		}
+	});
+});
+
+
