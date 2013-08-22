11854873a5882d1cf76a6707b8db98e6030b7586
Grant <grant.tepper@gmail.com>
2013-08-21 21:11:31 -0400

    adjusting map api

--- web/assets/js/map.js
+++ web/assets/js/map.js
@@ -1,10 +1,33 @@
 function initialize() {
-  var mapOptions = {
-    zoom: 13,
-    center: new google.maps.LatLng(42.886216,-78.873259),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
-  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+	var latlang = new google.maps.LatLng(42.886216,-78.873259);
+	var mapOptions = {
+		zoom: 13,
+		center: latlang,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+
+	var map = new google.maps.Map(
+		document.getElementById("map-canvas"), 
+		mapOptions
+	);
+
+	var marker = new google.maps.Marker({
+		map: map,
+		draggable: false,
+		animation: google.maps.Animation.DROP,
+		postion: latlang,
+		visable: true
+	});
+
+	google.maps.event.addListener(marker, 'click', toggleBounce);
+}
+
+function toggleBounce() {
+	if (marker.getAnimation() != null){ 
+		market.setAnimation(null);
+	} else { 
+		marker.setAnimation(google.maps.Animation.BOUNCE);
+	}
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
